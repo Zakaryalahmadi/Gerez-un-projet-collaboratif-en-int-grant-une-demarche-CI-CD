7e5845f14d6f84473062893ff0c786f29dd3461b
Zakarya Lahmadi <zakarya.lahmadi@affluences.com>
2025-07-12 18:54:22 +0200

    Refactor JokesService for improved code readability and formatting

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -4,23 +4,25 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
-
   private pathService = 'api/joke';
 
-  private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
+  private subject: BehaviorSubject<Joke | null> =
+    new BehaviorSubject<Joke | null>(null);
 
   constructor(private httpClient: HttpClient) {
     this.getRandomJoke();
   }
 
   public getRandomJoke(): void {
-    this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
+    this.httpClient
+      .get<Joke>(this.pathService)
+      .subscribe((joke: Joke) => this.subject.next(joke));
   }
 
-  public joke$(): Observable<Joke | null > {
-    return this.subject.asObservable();
+  public joke$(): Observable<Joke | null> {
+    return this.subject.asObservable(); //to remove
   }
 }
